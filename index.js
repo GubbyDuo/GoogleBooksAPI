f2debdb6c63dfb2054c28df64aace2499e073273
GubbyDuo <57397396+GubbyDuo@users.noreply.github.com>
2022-08-02 08:57:39 +1000

    added responsiveness

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const bookGrid = document.getElementById("booksGrid");
 const submitButton = document.getElementById("submitButton");
 
 submitButton.addEventListener("click", async () => {
-    console.log("test");
     bookGrid.innerHTML = "";
     const booklist = await getBookList();
-    console.log(booklist[1]);
     for (let i = 0; i < 12; i++) {
         const card = createCard(booklist, i);
         bookGrid.appendChild(card);
